2a9a1ccdd155eadc1a4979d096bd11b8a41e3dfa
hdt1996 <h.duong0818@gmail.com>
2022-10-07 08:37:43 -0700

    Pointer Notes

--- C++/pointers.cpp
+++ C++/pointers.cpp
@@ -3,6 +3,88 @@
 #include <string.h>
 #include <stdlib.h>
 
+namespace test
+{
+
+    void pa(const char *pa ) //same as char a[10]
+    {
+        printf("String: %s\n",pa);
+    }
+
+    void ppa(const char **ppa)   //Same as const char *ppa[]
+                            //No dimensional data aside from value data type
+    {
+        int i = 0;
+        int c = 0;
+        printf("%s -> Wrong: Trying to print pointer use %%p instead..\n",ppa); //Does not work because arg ppa is first element of original ppa
+                            //first element is a POINTER of type char** AKA POINTER to memory address to char
+                            //Need to dereference first element to get first address of underlying string array -> type char*
+                            //Can dereference again to get actual char values
+        printf("Memory address value of pointer: %p\n",(void*)ppa);
+        printf("Memory address actual pointer: %p\n",(void*)&ppa);
+        printf("%s\n",ppa[i]); //after dereferencing to first address of string array char*; valid for print
+        printf("String: %s=%s\n",*(ppa+i),ppa[i]);
+        printf("Value: %c=%c\n",*(*(ppa+i)+c),ppa[i][c]);
+        printf("Size of Actual Pointer: %lu\n",sizeof(&ppa)); //Need to use & since using ppa by itself signalled char**
+                                                            // sizeof needs to be (*) or single level pointer type, using & to achieve this
+        printf("Size of 1 Value: %lu\n",sizeof(ppa[i][c]));
+    }
+
+    void a(char *a ) //same as char a[10]
+    {
+        printf("String: %s\n",a);
+    }
+
+    void aa(char (*aa)[10] ) //same as char aa[10][10]
+                            //VALUE dimensions can be determined with sizeof(aa[i]) or sizeof(*(aa+i))
+                            //NOTE: EXACT SAME RESULTS AS aa_ptr2
+    {
+        int i = 0;
+        int c = 0;
+        printf("%s\n",aa); //works but flagged warning
+        printf("%s\n",aa[i]);
+        printf("String: %s=%s\n",*(aa+i),aa[i]);
+        printf("Value: %c=%c\n",*(*(aa+i)+c),aa[i][c]);
+        printf("Number of Values: %lu\n",sizeof(aa[i]));
+    }
+    
+    void aa_ptr1(char *aa_ptr1) //same as char aa_ptr1[]
+                            //VALUE dimensions can be determined with sizeof(aa[i]) or sizeof(*(aa+i))
+    {
+        int i = 0;
+        printf("%s\n",aa_ptr1); //works but flagged warning
+        printf("Value: %c=%c\n",*(aa_ptr1+i),aa_ptr1[i]);
+        printf("Size of 1 Value: %lu\n",sizeof(aa_ptr1[i]));
+    }
+    
+    void aa_ptr2(char (*aa_ptr2)[10]) //same as char aa_ptr2[10][10]
+                                    //pointer to array of size 10 with char
+                                    //NOTE: EXACT SAME RESULTS AS aa
+    {
+        int i = 0;
+        int c = 0;
+        printf("%s\n",aa_ptr2); //works but flagged warning
+        printf("%s\n",aa_ptr2[i]);
+        printf("Value: %c=%c\n",*(*(aa_ptr2+i)+c),aa_ptr2[i][c]);
+        printf("Size of 1 Value: %lu\n",sizeof(aa_ptr2[i]));
+    }
+
+    void aa_ptr3(char (*aa_ptr3)[10][10]) //same as char aa_ptr3[1][10][10]
+                                    //pointer to array of size 10 with char
+                                    //NOTE: EXACT SAME RESULTS AS aa
+    {
+        int a = 0;
+        int i = 0;
+        int c = 0;
+        printf("%s\n",aa_ptr3); //works but flagged warning
+        printf("%s\n",aa_ptr3[a][i]); //works but flagged warning
+        printf("Value: %c=%c\n",*(*(*(aa_ptr3+a)+i)+c),aa_ptr3[a][i][c]);
+        printf("Size of Extra: %lu\n",sizeof(aa_ptr3[a]));
+        printf("Size of Level 0: %lu\n",sizeof(aa_ptr3[a][i]));
+        printf("Size of 1 Value: %lu\n",sizeof(aa_ptr3[a][i][c]));
+    }
+}
+
 int main()
 {
     //Essential Rules:
@@ -25,9 +107,8 @@ int main()
     char a[10]="1234567"; //Declare array of size 8 with type char
     //char a[10]={"1234567"};
     char* a_ptr = a; // SAME AS char* a_ptr = &(a[0]);
-                    //Declare pointer TO type char
-                    //array always addressed with first 
-                    // We use decayed value -> this is a pointer (function-wise)
+                    // Declare pointer TO type char
+                    // Array value always DECAYS to first element's ADDRESS
                     // Sizeof is fruitless to get whole array size.
                     // sizeof(a_ptr) gives us pointer size
                     // If we dereference, we can only get size of actual data type stored in array
@@ -47,10 +128,12 @@ int main()
 
     char* aa_ptr1 = *aa;                //Declare pointer to first element of first subarray
                                         //This works because *aa is first element of aa deferenced.
-                                        // Although *aa is an array, the value is decayed to first address fo this array.
+                                        // Although *aa is first subarray, the value is decayed to first address of this subarray.
+                                        // Type of first address of subarray is type char(*) or pointer to char
 
     char (*aa_ptr2)[10] = &(*aa);       //Declare pointer to first subarray
-                                        // Here we use & of deferenced value (array). Here we need to match array dimensions
+                                        //Here we use & of deferenced value (array). Here we need to match array dimensions
+                                        //Value of aa_ptr2 is address of first subarray
 
     char (*aa_ptr3)[10][10] = &(aa);    //Declare pointer to whole array
                                         //Since we use & for original aa, we must match ALL dimensions
@@ -61,10 +144,45 @@ int main()
                                         // Whole string is printable without need for dimensions due to NULL terminator (Also present in above char*);
 
     const char *ppa[]= {"HUNG","HOANG","HIEP"};
-                                        //Declare array of size three with pointer to char
+                                        //Declare array of size three with pointer to char AKA array of pointers!
                                         // We do have dimensional data of how many subarrays there are (1)
-                                        // We have no dimension data of how many values unless we specify
+                                        // We have no dimension data of how many values in subarray unless we specify
                                         // Unless we point to an established array, no way to get exact number of values for char
                                         // We can set up a size as first index and use ppa++ after saving size and passing it to function
+    
+    // --------------------------------------------------FUNCTIONS---------------------------------------------
+    printf("test::a\n");
+    test::a(a); //arg: a is type char[10]; we are passing by value!
+                //a decays to char* since array decays to memory address of first element
+    printf("\ntest::aa\n");
+    test::aa(aa); //arg: aa is type char[10][10]; we are passing by value!
+                  //Since array decays to memory address of first element
+                  //First element of aa is subarray of 10 chars AKA char*[10]
+                  //Note: In function, when using subarray by name, the value is memory address of its first element which is char(*)
+                  //When we print, %s looks for char* datatype but subarray is not char*, it is char*[10]
+                  //EMPHASIZE: subarray is type char *[10] ==> array of memory addresses NOT char(*)[10] which signifies memory address to ARRAY
+                  //Print will still work but we will get flag. Way to fix is use subarray[i] | *(subarray+i)
+                  //Note that fix above prints the WHOLE word (increment memory address until NULL terminator)
+                  //To print individual characters of each word, dereference subarray (pull value by memory address incr) or index it
+    printf("\ntest::aa_ptr1\n");
+    test::aa_ptr1(aa_ptr1); //arg: aa_ptr is type char* AKA pointer to memory address of first element of first subarray
+                            //We can print whole word by %s due to increment until NULL terminator
+                            //REMEMBER: We are limited to only elements of FIRST subarray i.e. "Hung"
+                            //We cannot access other words, we will need to create more pointers
+    printf("\ntest::aa_ptr2\n");
+    test::aa_ptr2(aa_ptr2);
+
+    printf("\ntest::aa_ptr3\n");
+    test::aa_ptr3(aa_ptr3);
+
+    printf("\ntest::pa\n");
+    test::pa(pa);
+
+    printf("\ntest::ppa\n");
+    test::ppa(ppa);         //arg: ppa is array of pointers and will decay to first element address as function arg
+                            //first memory address implies char* already but since the elements are pointers, we add another *
+                            //Parameter for function will need to be char** or char* param []
+    printf("\n");
     return 0;
 }
+

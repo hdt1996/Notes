740723c587b83a6eef74558bc9b647fa3ee61d3e
hdt1996 <h.duong0818@gmail.com>
2022-11-16 04:21:56 -0800

    update to Java List

---
+++ Java/class_builder/templates/List.java
@@ -0,0 +1,19 @@
+package class_builder.templates;
+import java.util.*;
+
+class Chores{
+    public String due_date;
+    public String chore_name;
+    public String description;
+}
+
+class List{
+    public ArrayList<Chores> generateList(Map<String, String> chore_map){
+        ArrayList<Chores> myarrchore = new ArrayList<Chores>();
+        return myarrchore;
+
+    };
+    public void main(){
+
+    }
+}
